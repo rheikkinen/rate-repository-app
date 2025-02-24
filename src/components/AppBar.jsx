747040eb767cd427931252543e247fa2c5aa7aad
Rasmus Heikkinen <rasmus.heikkinen@outlook.com>
2025-02-24 11:09:22 +0200

    Add simple AppBar component

---
+++ src/components/AppBar.jsx
@@ -0,0 +1,35 @@
+import { View, StyleSheet, Pressable } from 'react-native';
+import Constants from 'expo-constants';
+import Text from './Text';
+import theme from '../theme';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: theme.colors.appBarBackground,
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  item: {
+    flexGrow: 1,
+    padding: 12,
+  },
+});
+
+const AppBarItem = ({ children, onPress }) => {
+  return (
+    <Pressable style={styles.item} onPress={onPress}>
+      <Text fontSize={'heading'}>{children}</Text>
+    </Pressable>
+  );
+};
+
+const AppBar = () => {
+  return (
+    <View style={styles.container}>
+      <AppBarItem onPress={() => {}}>Repositories</AppBarItem>
+    </View>
+  );
+};
+
+export default AppBar;
