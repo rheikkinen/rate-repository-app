ddf8e85aa5b2eb6d3f3171b5d6e415e6676cb5d0
Rasmus Heikkinen <rasmus.heikkinen@outlook.com>
2025-02-27 10:44:02 +0200

    Add custom hook for fetching repositories

---
+++ src/hooks/useRepositories.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+import Constants from 'expo-constants';
+
+const useRepositories = () => {
+  const [repositories, setRepositories] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const fetchRepositories = async () => {
+    setLoading(true);
+
+    const uri = Constants.expoConfig.extra.apiUri;
+    const response = await fetch(uri);
+    const json = await response.json();
+
+    setLoading(false);
+    setRepositories(json);
+  };
+
+  useEffect(() => {
+    fetchRepositories();
+  }, []);
+
+  return { repositories, loading, refetch: fetchRepositories };
+};
+
+export default useRepositories;
