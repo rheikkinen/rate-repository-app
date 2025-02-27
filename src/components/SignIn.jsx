7b8de4282db1927d8ec8f007cbc59b59f7b7b28e
Rasmus Heikkinen <rasmus.heikkinen@outlook.com>
2025-02-27 14:23:43 +0200

    Add useSignIn hook to handle authenticate mutation
    Use the hook in the sign-in form.

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -5,6 +5,7 @@ import Button from './Button';
 import Text from './Text';
 
 import * as yup from 'yup';
+import useSignIn from '../hooks/useSignIn';
 
 const validationSchema = yup.object().shape({
   username: yup.string().required('Username is required'),
@@ -101,8 +102,17 @@ const SignInForm = ({ onSubmit }) => {
 };
 
 const SignIn = () => {
-  const onSubmit = (values) => {
-    console.log(values);
+  const [signIn] = useSignIn();
+
+  const onSubmit = async (values) => {
+    const { username, password } = values;
+
+    try {
+      const { data } = await signIn({ username, password });
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return <SignInForm onSubmit={onSubmit} />;
