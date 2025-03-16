6c8fa9a18aeb660ff109d8b69815e762d35bc6d8
Rasmus Heikkinen <rasmus.heikkinen@outlook.com>
2025-03-16 16:24:06 +0200

    Add navigation to home on user sign-out

--- src/hooks/useSession.js
+++ src/hooks/useSession.js
@@ -1,6 +1,7 @@
 import { useApolloClient, useQuery } from '@apollo/client';
-import { ME } from '../graphql/queries';
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-native';
+import { ME } from '../graphql/queries';
 import useAuthStorage from './useAuthStorage';
 
 const useSession = () => {
@@ -10,6 +11,7 @@ const useSession = () => {
   const [currentUser, setCurrentUser] = useState(null);
   const authStorage = useAuthStorage();
   const apolloClient = useApolloClient();
+  const navigateTo = useNavigate();
 
   useEffect(() => {
     if (!loading) {
@@ -21,6 +23,7 @@ const useSession = () => {
     setCurrentUser(null);
     await authStorage.removeAccessToken();
     apolloClient.resetStore();
+    navigateTo('/');
   };
 
   return { currentUser, loading, signOut };
